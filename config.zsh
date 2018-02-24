c4209dec8cd077919230720f0283d81804bdb306
Madkita <abubakaryagob@gmail.com>
2018-02-25 00:33:06 +0800

    added coloring to dict alias

--- config.zsh
+++ config.zsh
@@ -72,7 +72,7 @@ alias zsh="exec zsh"
 alias sysinfo="sh /run/media/dark-emperor/Dark-Files/Stuff/Others/Git/Scripts/sysinfo.sh"
 alias open="xdg-open"
 alias term="termite &" #nedded sometimes.
-alias dict="sdcv" # for sdcv dictionary
+alias dict="sdcv -c" # for sdcv dictionary
 
 #packages aliases.
 alias remove="sudo pacman -Rs"
