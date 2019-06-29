d3790b0686fef3787c79f6b30c34e43e6cb5c824
Blacksuan19 <Abubakaryagob@gmail.com>
2019-06-29 23:30:43 +0800

    playing: add different icons for playing and paused
    Signed-off-by: Blacksuan19 <Abubakaryagob@gmail.com>

--- bin/playing.sh
+++ bin/playing.sh
@@ -2,7 +2,9 @@
 
 if pgrep -x "spotify" > /dev/null
 then
-ARTIST=$(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 \
+    STATUS=$(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'PlaybackStatus'|egrep -A 1 "string"|cut -b 26-|cut -d '"' -f 1)
+
+    ARTIST=$(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 \
             org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' \
             string:'Metadata' |\
             awk -F 'string "' '/string|array/ {printf "%s",$2; next}{print ""}' |\
@@ -14,7 +16,12 @@ ARTIST=$(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpri
             awk -F 'string "' '/string|array/ {printf "%s",$2; next}{print ""}' |\
             awk -F '"' '/title/ {print $2}')
 
-echo $SONG by $ARTIST
+    if [[ $STATUS = "Playing" ]]; then
+    echo  $SONG by $ARTIST
+    else
+    echo  $SONG by $ARTIST
+    fi
+    
 else
     echo Spotify is not running
 fi
