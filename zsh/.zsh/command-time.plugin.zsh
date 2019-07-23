fbec7fb13565cf8eccbf60e8da844a075c0adb3b
Blacksuan19 <Abubakaryagob@gmail.com>
2019-07-23 13:58:45 +0800

    Make compaitable with stow
    Signed-off-by: Blacksuan19 <Abubakaryagob@gmail.com>

---
+++ zsh/.zsh/command-time.plugin.zsh
@@ -0,0 +1,26 @@
+_command_time_preexec() {
+  timer=${timer:-$SECONDS}
+  export ZSH_COMMAND_TIME=""
+}
+
+_command_time_precmd() {
+  if [ $timer ]; then
+    timer_show=$(($SECONDS - $timer))
+    if [ -n "$TTY" ] && [ $timer_show -ge ${ZSH_COMMAND_TIME_MIN_SECONDS:-3} ]; then
+      export ZSH_COMMAND_TIME="$timer_show"
+      if [ -n ${ZSH_COMMAND_TIME_ECHO+1} ] && [ -n "$ZSH_COMMAND_TIME_ECHO" ]; then
+        zsh_command_time
+      fi
+    fi
+    unset timer
+  fi
+}
+
+zsh_command_time() {
+  if [ -n "$ZSH_COMMAND_TIME" ]; then
+  	notify-send --icon="utilities-terminal" "Command Finished" "Running Command Finished In $ZSH_COMMAND_TIME Seconds"
+  fi
+}
+
+precmd_functions+=(_command_time_precmd)
+preexec_functions+=(_command_time_preexec)
