1759a5e6de7cf5cee69a38a13fc8d359089dc893
Blacksuan19 <Abubakaryagob@gmail.com>
2019-11-02 18:51:59 +0800

    dots: back on regullar spotify
    well it was good while it lasted, reason for this is that spotifyd is
    really unstable, random crashes and mpris interface is as well, not to
    mention the delay while switching/stopping or playing songs and another
    reason is if spotifyd is trippin spotify-tui will do the same
    
    Signed-off-by: Blacksuan19 <Abubakaryagob@gmail.com>

--- bin/bin/playing.sh
+++ bin/bin/playing.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 
-if pgrep -x "spotifyd" > /dev/null
+if pgrep -x "spotify" > /dev/null
 then
-    STATUS=$(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotifyd /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'PlaybackStatus'|egrep -A 1 "string"|cut -b 26-|cut -d '"' -f 1)
+        STATUS=$(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'PlaybackStatus'|egrep -A 1 "string"|cut -b 26-|cut -d '"' -f 1)
 
-    if [[ $STATUS =~ "Error" ]]; then
-        echo " spotifyd is dead, retarting"
-        spd=$(pidof spotifyd)
-        kill $spd
-    fi
+        if [[ $STATUS =~ "Error" ]]; then
+            echo " spotifyd is dead, retarting"
+            spd=$(pidof spotifyd)
+            kill $spd
+        fi
 
-    ARTIST=$(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotifyd /org/mpris/MediaPlayer2 \
+        ARTIST=$(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 \
             org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' \
             string:'Metadata' |\
             awk -F 'string "' '/string|array/ {printf "%s",$2; next}{print ""}' |\
             awk -F '"' '/artist/ {print $2}')
 
-   SONG=$(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotifyd /org/mpris/MediaPlayer2 \
+        SONG=$(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 \
             org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' \
             string:'Metadata' |\
             awk -F 'string "' '/string|array/ {printf "%s",$2; next}{print ""}' |\
             awk -F '"' '/title/ {print $2}' | sed -e 's/([^()]*)//g' | cut -c 1-20 )
 
-    if [[ $STATUS = "Playing" ]]; then
-    echo " $SONG BY $ARTIST"
-    else
-    echo " $SONG BY $ARTIST"
-    fi
+        if [[ $STATUS = "Playing" ]]; then
+        echo " $SONG - $ARTIST"
+        else
+        echo " $SONG - $ARTIST"
+        fi
     # mute the audio if an ad is playing(too broke for premium now)
-else
-    echo " Spotify is not running"
-fi
+    else
+        echo " Spotify is not running"
+    fi
