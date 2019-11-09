25098cef6d7da6068f6f96ac6feff6e9d60bb35f
blacksuan19 <Abubakaryagob@gmail.com>
2019-11-09 16:06:47 +0800

    zsh: embrace the void
    - remove systemd aliases (oh yeah)
    - replace yay aliases with xbps
    - set new hashes (this is a much easier mount point)
    - cleanup sources
    - replace lsd with regular ls (add colors)
    
    Signed-off-by: blacksuan19 <Abubakaryagob@gmail.com>

--- zsh/.zsh/config.zsh
+++ zsh/.zsh/config.zsh
@@ -1,20 +1,5 @@
-# systemd aliases.
- user_commands=(
-  list-units is-active status show help list-unit-files
-  is-enabled list-jobs show-environment cat list-timers)
-
-sudo_commands=(
-  start stop reload restart try-restart isolate kill
-  reset-failed enable disable reenable preset mask unmask
-  link load cancel set-environment unset-environment
-  edit)
-
-for c in $user_commands; do; alias sc-$c="systemctl $c"; done
-for c in $sudo_commands; do; alias sc-$c="sudo systemctl $c"; done
-
-alias sc-enable-now="sc-enable --now"
-alias sc-disable-now="sc-disable --now"
-alias sc-mask-now="sc-mask --now"
+#ls colors
+eval $( dircolors -b $HOME/.dircolors )
 
 # double press Esc to add sudo.
 sudo-command-line() {
@@ -36,15 +21,10 @@ bindkey "\e\e" sudo-command-line
 export EDITOR=nvim
 # various aliases.
 alias free="free -h"
-alias network="sc-restart NetworkManager"
-alias blame="systemd-analyze && systemd-analyze blame"
-alias journal="journalctl -b0 -p err"
 alias fdisk="sudo fdisk -l"
 alias cleanj="sudo journalctl --vacuum-time=5d"
 alias cpr="rsync --progress --size-only --inplace --verbose "
 alias cat="bat " # this one is way better
-alias ls="lsd"
-alias l="lsd -al"
 alias zsh="exec zsh"
 alias open="xdg-open 2>/dev/null"
 alias poly="killall polybar && polybar main </dev/null &>/dev/null &"
@@ -54,12 +34,9 @@ alias n="nvim"
 alias study="cd ~kul && ranger"
 
 #packages aliases.
-alias y="yay"
-alias remove="yay -Rs"
-alias install="yay -S"
-alias pinfo="yay -Qi " # get info of an installed package.
-alias orphan="yay -Rns $(pacman -Qtdq)" # remove orphaned packages.
-alias cleanc="yay -Scc" # clean cached packages files.
+alias install="sudo xbps-install "
+alias remove="sudo xbps-remove"
+alias pinfo="xbps-query " # get info of an installed package.
 
 #spotify aliases.
 alias spn="sp next"
@@ -81,14 +58,14 @@ alias gcp="git cherry-pick"
 alias gl="git log | bat"
 
 # Directory hashes.
-hash -d disk="/run/media/Dark-Files"
-hash -d idea="/run/media/Dark-Files/IdeaProjects"
-hash -d sd="/run/media/Dark-Files/SD Card/"
+hash -d disk="/media/Dark-Files"
+hash -d idea="/media/Dark-Files/IdeaProjects"
+hash -d sd="/media/Dark-Files/SD Card/"
 hash -d dots="/home/blacksuan19/.dotfiles"
-hash -d git="/run/media/Dark-Files/Gits"
-hash -d android="/run/media/Dark-Files/Android-DEV"
-hash -d da="/run/media/Dark-Files/Android-DEV/Dark-Ages"
-hash -d trees="/run/media/Dark-Files/Android-DEV/Trees"
-hash -d flutter="/run/media/Dark-Files/Flutter"
-hash -d kul="/run/media/Dark-Files/Kulliyya"
+hash -d git="/media/Dark-Files/Gits"
+hash -d android="/media/Dark-Files/Android-DEV"
+hash -d da="/media/Dark-Files/Android-DEV/Dark-Ages"
+hash -d trees="/media/Dark-Files/Android-DEV/Trees"
+hash -d flutter="/media/Dark-Files/Flutter"
+hash -d kul="/media/Dark-Files/Kulliyya"
 ufetch # some fancy shit in the begining
