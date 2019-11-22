969bdb39ad0d00654a297a0d6d924c4c41dc34e4
blacksuan19 <Abubakaryagob@gmail.com>
2019-11-22 23:17:56 +0800

    playing: add support for cmus
    is time to play back those mixes
    
    Signed-off-by: blacksuan19 <Abubakaryagob@gmail.com>

--- bin/bin/playing.sh
+++ bin/bin/playing.sh
@@ -1,32 +1,28 @@
 #!/bin/bash
 
-if pgrep -x "spotify" > /dev/null
+if pgrep -x "spotify" || pgrep -x "cmus" > /dev/null
 then
-        STATUS=$(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'PlaybackStatus'|egrep -A 1 "string"|cut -b 26-|cut -d '"' -f 1)
+    PID=$(pgrep -x "spotify" || pgrep -x "cmus")
+    PLAYER=$(pmap $PID | head -1 | awk '{print $2}')
+    STATUS=$(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.$PLAYER /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'PlaybackStatus'|egrep -A 1 "string"|cut -b 26-|cut -d '"' -f 1)
 
-        if [[ $STATUS =~ "Error" ]]; then
-            echo " spotifyd is dead, retarting"
-            spd=$(pidof spotifyd)
-            kill $spd
-        fi
+    ARTIST=$(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.$PLAYER /org/mpris/MediaPlayer2 \
+        org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' \
+        string:'Metadata' |\
+        awk -F 'string "' '/string|array/ {printf "%s",$2; next}{print ""}' |\
+        awk -F '"' '/artist/ {print $2}')
 
-        ARTIST=$(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 \
-            org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' \
-            string:'Metadata' |\
-            awk -F 'string "' '/string|array/ {printf "%s",$2; next}{print ""}' |\
-            awk -F '"' '/artist/ {print $2}')
+    SONG=$(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.$PLAYER /org/mpris/MediaPlayer2 \
+        org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' \
+        string:'Metadata' |\
+        awk -F 'string "' '/string|array/ {printf "%s",$2; next}{print ""}' |\
+        awk -F '"' '/title/ {print $2}' | sed -e 's/([^()]*)//g' | cut -c 1-20 )
 
-        SONG=$(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 \
-            org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' \
-            string:'Metadata' |\
-            awk -F 'string "' '/string|array/ {printf "%s",$2; next}{print ""}' |\
-            awk -F '"' '/title/ {print $2}' | sed -e 's/([^()]*)//g' | cut -c 1-20 )
-
-        if [[ $STATUS = "Playing" ]]; then
-        echo " $SONG - $ARTIST"
-        else
-        echo " $SONG - $ARTIST"
-        fi
+    if [[ $STATUS = "Playing" ]]; then
+    echo " $SONG - $ARTIST"
+    else
+    echo " $SONG - $ARTIST"
+    fi
     # mute the audio if an ad is playing(too broke for premium now)
     else
         echo " Spotify is not running"
