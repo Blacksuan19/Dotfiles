cd9f50a74205a781689a913b3129c07612b24b05
blacksuan19 <Abubakaryagob@gmail.com>
2019-11-23 23:39:00 +0800

    playing: fix spotify detection
    - spotify spawns multiple processes so only use one of them
    - pmap cant be used because it gives the full path of the command instead of just program name
    - flip the pid checks (otherwise cmus will be broken)
    - more approriate wording
    
    Signed-off-by: blacksuan19 <Abubakaryagob@gmail.com>

--- bin/bin/playing.sh
+++ bin/bin/playing.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 
-if pgrep -x "spotify" || pgrep -x "cmus" > /dev/null
+if pgrep -x "spotify" > /dev/null || pgrep -x "cmus" > /dev/null
 then
-    PID=$(pgrep -x "spotify" || pgrep -x "cmus")
-    PLAYER=$(pmap $PID | head -1 | awk '{print $2}')
+    PID=$(pgrep -x "cmus" || pgrep -x "spotify" | head -1)
+    PLAYER=$(ps -p $PID -o comm=)
     STATUS=$(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.$PLAYER /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'PlaybackStatus'|egrep -A 1 "string"|cut -b 26-|cut -d '"' -f 1)
 
     ARTIST=$(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.$PLAYER /org/mpris/MediaPlayer2 \
@@ -18,12 +18,11 @@ then
         awk -F 'string "' '/string|array/ {printf "%s",$2; next}{print ""}' |\
         awk -F '"' '/title/ {print $2}' | sed -e 's/([^()]*)//g' | cut -c 1-20 )
 
-    if [[ $STATUS = "Playing" ]]; then
+    if [ $STATUS = "Playing" ]; then
     echo " $SONG - $ARTIST"
     else
     echo " $SONG - $ARTIST"
     fi
-    # mute the audio if an ad is playing(too broke for premium now)
     else
-        echo " Spotify is not running"
+        echo " No player is running"
     fi
