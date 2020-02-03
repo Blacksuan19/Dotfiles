bb7f8bbf00a78643469a5a82cba1425adeff2ca0
blacksuan19 <abubakaryagob@gmail.com>
2020-02-04 05:38:08 +0800

    zsh: major cleanup
    - cleanup prezto config
    - update powerlevel10k theme config
    - remove unused sources from zshrc
    - remove command notfound module (was slowing down completion)
    - remove syntax-highlighting module (we use the faster version)
    - set key-bindings in .zpreztorc
    - move double press esc for sudo function to functions
    - remove rsync alias (meh)
    - remove terminal module (i dont have a title bar)
    
    Signed-off-by: blacksuan19 <i@blacksuan19.me>

--- zsh/.zsh/functions.zsh
+++ zsh/.zsh/functions.zsh
@@ -1,3 +1,21 @@
+# double press Esc to add sudo.
+sudo-command-line() {
+    [[ -z $BUFFER ]] && zle up-history
+    if [[ $BUFFER == sudo\ * ]]; then
+        LBUFFER="${LBUFFER#sudo }"
+    elif [[ $BUFFER == $EDITOR\ * ]]; then
+        LBUFFER="${LBUFFER#$EDITOR }"
+        LBUFFER="sudoedit $LBUFFER"
+    elif [[ $BUFFER == sudoedit\ * ]]; then
+        LBUFFER="${LBUFFER#sudoedit }"
+        LBUFFER="$EDITOR $LBUFFER"
+    else
+        LBUFFER="sudo $LBUFFER"
+    fi
+}
+zle -N sudo-command-line
+bindkey "\e\e" sudo-command-line
+
 # extract a file no matter the format
 ex() {
     local c e i
