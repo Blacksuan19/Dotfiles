e4b8e8561c6435dd1963e748af9f56473eaebb74
blacksuan19 <i@blacksuan19.me>
2020-02-04 05:38:08 +0800

    zsh: major cleanup
    - cleanup prezto config
    - update powerlevel10k theme config
    - remove unused sources from zshrc
    - remove command notfound module (was slowing down completion)
    - remove syntax-highlighting module (we use the faster version)
    - set key-bindings in .zpreztorc
    - move double press esc for sudo function to functions
    - remove rsync alias (meh)
    - remove terminal module (i dont have a title bar)
    
    Signed-off-by: blacksuan19 <i@blacksuan19.me>

--- zsh/.zsh/config.zsh
+++ zsh/.zsh/config.zsh
@@ -1,36 +1,17 @@
-# enable vi mode
-bindkey -v
-
 # ls colors
 eval $( dircolors -b $HOME/.dircolors )
 
-# double press Esc to add sudo.
-sudo-command-line() {
-    [[ -z $BUFFER ]] && zle up-history
-    if [[ $BUFFER == sudo\ * ]]; then
-        LBUFFER="${LBUFFER#sudo }"
-    elif [[ $BUFFER == $EDITOR\ * ]]; then
-        LBUFFER="${LBUFFER#$EDITOR }"
-        LBUFFER="sudoedit $LBUFFER"
-    elif [[ $BUFFER == sudoedit\ * ]]; then
-        LBUFFER="${LBUFFER#sudoedit }"
-        LBUFFER="$EDITOR $LBUFFER"
-    else
-        LBUFFER="sudo $LBUFFER"
-    fi
-}
-zle -N sudo-command-line
-bindkey "\e\e" sudo-command-line
+# default editor
 export EDITOR=nvim
 
 # determines search program for fzf
 if type rg &> /dev/null; then
     export FZF_DEFAULT_COMMAND='rg --files --hidden""'
 fi
+
 # various aliases.
 alias free="free -h"
 alias fdisk="sudo fdisk -l"
-alias cpr="rsync --progress --size-only --inplace --verbose "
 alias cat="bat " # this one is way better
 alias zsh="exec zsh"
 alias open="xdg-open 2>/dev/null"
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -1,16 +1,7 @@
-# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
-# Initialization code that may require console input (password prompts, [y/n]
-# confirmations, etc.) must go above this block, everything else may go below.
-if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
-  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
-fi
-
 # only sourcing here!!
 source /home/blacksuan19/.zprezto/init.zsh
 export PATH=$HOME/bin:$PATH
-export PATH=${PATH}:/home/blacksuan19/.gem/ruby/2.6.0/bin:$PATH
 export PATH=${PATH}:/home/blacksuan19/.gem/ruby/2.7.0/bin:$PATH
-export PATH=${PATH}:/home/blacksuan19/go/bin:$PATH
 source /home/blacksuan19/.zsh/fast/fast-syntax-highlighting.plugin.zsh
 source /home/blacksuan19/.zsh/config.zsh
 source /home/blacksuan19/.zsh/functions.zsh
