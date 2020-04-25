7f54bd46a99a8df66acab3021bd5dc5258f87f0b
blacksuan19 <i@blacksuan19.me>
2020-04-25 17:25:11 +0000

    unsplash: blur image before making a wall
    Signed-off-by: blacksuan19 <i@blacksuan19.me>

--- bin/bin/unsplash.sh
+++ bin/bin/unsplash.sh
@@ -1,11 +1,13 @@
 #/bin/bash
 
 # get a random 1080p wallpaper from unsplash.com whenever this script is run
-# depends on notify-send for notifications
+# depends on notify-send for notifications, imagemagick for blur effect
 # run hourly with cron:
 # @hourly $HOME/bin/unsplash.sh
 
 notify-send "Getting new wallpaper..."
 wget -O /tmp/wallpaper.jpg https://unsplash.it/1920/1080/?random &> /dev/null
-feh --bg-fill /tmp/wallpaper.jpg
+# apply some light blur first
+convert /tmp/wallpaper.jpg -filter Gaussian -blur 0x2 /tmp/blur-wall.jpg
+feh --bg-fill /tmp/blur-wall.jpg
 notify-send "New wallpaper set successfully."
