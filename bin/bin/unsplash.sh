6d660d5695452d74907e24be5d0ba798de9fed90
blacksuan19 <i@blacksuan19.me>
2020-04-25 21:16:16 +0000

    unsplash: udpdate betterlockscreen wallaper as well
    Signed-off-by: blacksuan19 <i@blacksuan19.me>

--- bin/bin/unsplash.sh
+++ bin/bin/unsplash.sh
@@ -7,7 +7,15 @@
 
 notify-send "Getting new wallpaper..."
 wget -O $HOME/.wallpaper.jpg "https://source.unsplash.com/random/1920x1080/?wallpaper" &> /dev/null
+
 # apply some light blur first
 # convert $HOME/.wallpaper.jpg -filter Gaussian -blur 0x2 $HOME/.blur-wall.jpg
+
+# set bg via feh so we can restore it on boot
 feh --bg-fill $HOME/.wallpaper.jpg
+
+# update betterlockscreen wallpaper
+betterlockscreen -u $HOME/.wallpaper.jpg dimblur &> /dev/null
+
+#we out
 notify-send "New wallpaper set successfully."
