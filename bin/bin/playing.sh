3aaf85c930a6fc6717de917a13abbb999afd24b4
blacksuan19 <i@blacksuan19.me>
2020-04-25 21:04:46 +0000

    playing: cleanup the output
    - remove extra artists (in () usually)
    - limit to 50 chars
    - avoid repeating code
    - use a switch statement for keks
    - set module maxlen to 35
    
    Signed-off-by: blacksuan19 <i@blacksuan19.me>

--- bin/bin/playing.sh
+++ bin/bin/playing.sh
@@ -2,10 +2,18 @@
 
 STATUS=$(playerctl status 2> /dev/null)
 
-if [ "$STATUS" = "Playing" ]; then
-    echo "  $(playerctl metadata artist) - $(playerctl metadata title)"
-elif [ "$STATUS" = "Paused" ]; then
-    echo "  $(playerctl metadata artist) - $(playerctl metadata title)"
-else
+#TODO find a better way to handle not running case
+if [ "$STATUS" != "Playing" ] && [ "$STATUS" != "Paused" ]; then
     echo " No player is running"
+else
+    P_ICON=""
+    S_ICON=""
+    METADATA="$(playerctl metadata artist) - $(playerctl metadata title)"
+    TRIM=$(echo $METADATA | sed -e 's/([^()]*)//g' | cut -c 1-50) 
+    case $STATUS in
+        "Playing")
+            echo $P_ICON $TRIM;;
+        "Paused")
+            echo $S_ICON $TRIM;;
+    esac
 fi
