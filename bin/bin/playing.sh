387f4687dae1a3e3974768ed94a4dc9c32c5fcb6
blacksuan19 <i@blacksuan19.me>
2020-05-03 03:53:56 +0800

    playing: check if spotify is playing on another device
    if it is, both artist and song will be empty string, print something to
    let us know that is the case
    
    Signed-off-by: blacksuan19 <i@blacksuan19.me>

--- bin/bin/playing.sh
+++ bin/bin/playing.sh
@@ -2,7 +2,6 @@
 
 STATUS=$(playerctl status 2> /dev/null)
 
-#TODO find a better way to handle not running case
 if [ "$STATUS" != "Playing" ] && [ "$STATUS" != "Paused" ]; then
     echo " No player is running"
 else
@@ -10,10 +9,21 @@ else
     S_ICON=""
     METADATA="$(playerctl metadata artist) - $(playerctl metadata title)"
     TRIM=$(echo $METADATA | sed -e 's/([^()]*)//g' | cut -c 1-50) 
+    ARTIST=$(playerctl metadata artist)
+    FULL_META=$(playerctl metadata)
     case $STATUS in
         "Playing")
-            echo $P_ICON $TRIM;;
+            # if spotify is playing on another device artist and song will be empty
+            if [[ "$ARTIST" == "" ]] && [[ "$FULL_META" =~ "spotify" ]]; then
+                echo $P_ICON "Playing on Another Device"
+            else
+                echo $P_ICON $TRIM
+            fi;;
         "Paused")
-            echo $S_ICON $TRIM;;
+            if [[ "$ARTIST" == "" ]] && [[ "$FULL_META" =~ "spotify" ]]; then
+                echo $S_ICON "Paused on Another Device"
+            else
+                echo $S_ICON $TRIM
+            fi;;
     esac
 fi
