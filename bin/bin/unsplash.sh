cc5360ac86f6b9aab6ad543c21f94e9f21b2e50b
blacksuan19 <abubakaryagob@gmail.com>
2020-05-04 00:00:52 +0800

    unsplash: set wallpaper using betterlockscreen
    this allows setting a blurred version of the image generated by
    betterlockscreen, also makes the script cleaner
    
    Signed-off-by: blacksuan19 <i@blacksuan19.me>

--- bin/bin/unsplash.sh
+++ bin/bin/unsplash.sh
@@ -1,21 +1,19 @@
 #/bin/bash
-
+# copyright © blacksuan19 @ 2020
 # get a random 1080p wallpaper from unsplash.com whenever this script is run
-# depends on notify-send for notifications, imagemagick for blur effect
+# depends on notify-send for notifications, betterlockscreen for setting desktop and
+# lockscreen wallpapers
 # run hourly with cron:
 # @hourly $HOME/bin/unsplash.sh
 
 notify-send "Getting new wallpaper..."
 wget --no-check-certificate -O $HOME/.wallpaper "https://source.unsplash.com/random/1920x1080/?wallpaper" &> /dev/null
 
-# apply some light blur first
-# convert $HOME/.wallpaper.jpg -filter Gaussian -blur 0x2 $HOME/.blur-wall.jpg
-
-# set bg via feh so we can restore it on boot
-feh --bg-fill $HOME/.wallpaper
-
 # update betterlockscreen wallpaper
-betterlockscreen -u $HOME/.wallpaper dimblur &> /dev/null
+betterlockscreen -u $HOME/.wallpaper blur &> /dev/null
+
+# set desktop wallpaper(add `blur` to use the blurred version)
+betterlockscreen -w
 
 #we out
 notify-send "New wallpaper set successfully."
