e7401696ff07ce81f338d88496faf7caed7da5d4
blacksuan19 <abubakaryagob@gmail.com>
2020-05-06 07:59:45 +0800

    keybindings: move to rofi_run
    - no need for a separate script
    - just check $1 in rofi_run (always one arg)
    - fix sleep command not working
    - no need to specify full path in sxhkdrc (~/bin is in path)
    
    Signed-off-by: blacksuan19 <i@blacksuan19.me>

--- bin/bin/keybindings.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# a simple script to show sxhkd Keybindings in a searchable rofi window
-# main inspration https://www.reddit.com/r/bspwm/comments/aejyze/tip_show_sxhkd_keybindings_with_fuzzy_search/
-
-awk '/\w/ && last { print $0,"\t",last} {last=""} /^#/{gsub("#","");last=$0}' \
-~/.config/sxhkd/sxhkdrc | column -t -s $'\t' | rofi -dmenu -i -P "Keybindings:" -width 70
