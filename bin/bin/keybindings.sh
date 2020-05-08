de7e4e13fee25f347bd6dad2e943adc9cfb84a14
blacksuan19 <abubakaryagob@gmail.com>
2020-05-09 01:09:05 +0800

    keybindings: remove # in front of description
    kinda annoyed me
    
    Signed-off-by: blacksuan19 <i@blacksuan19.me>

--- bin/bin/keybindings.sh
+++ bin/bin/keybindings.sh
@@ -3,5 +3,5 @@
 # main inspration https://www.reddit.com/r/bspwm/comments/aejyze/tip_show_sxhkd_keybindings_with_fuzzy_search/
 
 cat ~/.config/sxhkd/sxhkdrc \
-| awk '/\w/ && last {print $0,"\t",last} {last=""} /^#/{last=$0}' \
+| awk '/\w/ && last { print $0,"\t",last} {last=""} /^#/{gsub("#","");last=$0}' \
 | column -t -s $'\t' | rofi -dmenu -i -P "Keybindings:" -width 70
