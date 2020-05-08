a491f61a6521ec57b6b0d2f5ef7b9e03dc8da016
blacksuan19 <abubakaryagob@gmail.com>
2020-05-09 01:42:29 +0800

    keybindings: why cat when you can just awk
    Signed-off-by: blacksuan19 <i@blacksuan19.me>

--- bin/bin/keybindings.sh
+++ bin/bin/keybindings.sh
@@ -2,6 +2,5 @@
 # a simple script to show sxhkd Keybindings in a searchable rofi window
 # main inspration https://www.reddit.com/r/bspwm/comments/aejyze/tip_show_sxhkd_keybindings_with_fuzzy_search/
 
-cat ~/.config/sxhkd/sxhkdrc \
-| awk '/\w/ && last { print $0,"\t",last} {last=""} /^#/{gsub("#","");last=$0}' \
-| column -t -s $'\t' | rofi -dmenu -i -P "Keybindings:" -width 70
+awk '/\w/ && last { print $0,"\t",last} {last=""} /^#/{gsub("#","");last=$0}' \
+~/.config/sxhkd/sxhkdrc | column -t -s $'\t' | rofi -dmenu -i -P "Keybindings:" -width 70
