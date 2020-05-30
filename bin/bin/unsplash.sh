bb35503d0f2fc2d1ae81c7ba2aad5d6cf8b395f6
blacksuan19 <i@blacksuan19.me>
2020-05-31 04:02:13 +0800

    unsplash: dont specify search term
    not that many images tagged as wallpaper
    
    Signed-off-by: blacksuan19 <i@blacksuan19.me>

--- bin/bin/unsplash.sh
+++ bin/bin/unsplash.sh
@@ -1,12 +1,12 @@
 #/bin/bash
-# copyright © blacksuan19 @ 2020
+# copyright © 2020 blacksuan19
 # get a random 1080p wallpaper from unsplash.com whenever this script is run
 # depends on notify-send for notifications, feh for setting wallpaper
 # run hourly with cron:
 # @hourly $HOME/bin/unsplash.sh
 
 notify-send "Getting new wallpaper..."
-wget --no-check-certificate -O $HOME/.wallpaper "https://source.unsplash.com/random/1920x1080/?wallpaper" &> /dev/null
+wget --no-check-certificate -O $HOME/.wallpaper "https://source.unsplash.com/random/1920x1080" &> /dev/null
 
 # set desktop wallpaper
 feh --bg-fill $HOME/.wallpaper
