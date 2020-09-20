3988282d43a5ee10fe73595a772725a3640ea405
blacksuan19 <abubakaryagob@gmail.com>
2020-09-20 18:13:39 +0800

    install: auto download & install fonts
    Signed-off-by: blacksuan19 <i@blacksuan19.me>

--- install.sh
+++ install.sh
@@ -1,20 +1,50 @@
 #!/usr/bin/env bash
 
-# ignored files list
-declare -a ignore_list=(".git"
-                        ".gitignore"
-                        ".gitmodules"
-                        "README.md"
-                        "screen1.png"
-                        "screen2.png"
-                        "screen3.png"
-                        "screen4.png"
-                    )
+# install required fonts
+function install_fonts() {
+    target="$HOME/.local/share/fonts"
+    mkdir -p $target
 
-# go throw all files except ignore list
-for file in ~/.dotfiles/*; do
-  if [ -d ${file} ] && [[ ! ${file} =~ ${ignore_list[@]} ]]; then
-    stow $(basename $file)
-    if $SCRIPT_DEBUG; then echo "$(basename $file) stowed."; fi
-  fi
-done
+    # install apple SanFranciscoDisplay font
+    curl https://codeload.github.com/AppleDesignResources/SanFranciscoFont/zip/master \
+       --output SF.zip
+    unzip -j SF.zip SanFranciscoFont-master/SanFranciscoDisplay-\* -d $target/SanFranciscoDisplay
+
+    # install JetBrainsMono
+    curl https://download-cf.jetbrains.com/fonts/JetBrainsMono-2.001.zip --output \
+        JetBrainsMono.zip
+    unzip -j JetBrainsMono.zip ttf\/JetBrainsMono\* -d $target/JetBrainsMono
+
+    # cleanup
+    rm SF.zip JetBrainsMono.zip
+
+    # install JetBrainsMono nerd font
+    curl https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/JetBrainsMono/Regular/complete/JetBrains%20Mono%20Regular%20Nerd%20Font%20Complete.ttf \
+        --output $target/JetBrainsMono/JetBrains\ Mono\ Regular\ Nerd\ Font\ Complete.ttf
+
+}
+
+# symlink configs
+function stow_con() {
+    # ignored files list
+    declare -a ignore_list=(".git"
+                            ".gitignore"
+                            ".gitmodules"
+                            "README.md"
+                            "screen1.png"
+                            "screen2.png"
+                            "screen3.png"
+                            "screen4.png"
+                        )
+
+    # go throw all files except ignore list
+    for file in ~/.dotfiles/*; do
+      if [ -d ${file} ] && [[ ! ${file} =~ ${ignore_list[@]} ]]; then
+        stow $(basename $file)
+        if $SCRIPT_DEBUG; then echo "$(basename $file) stowed."; fi
+      fi
+    done
+}
+
+stow_con
+install_fonts
