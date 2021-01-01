503a516125c7850bd3ce521b037e693b49070e12
blacksuan19 <i@blacksuan19.me>
2021-01-02 00:40:00 +0800

    playing.sh: show status for only currently playing player
    - fixes an edge case where a chrome based browser would not kill its
    instance leading to %all players being marked as stopped
    - makes it possible to track actually only playing players
    - prioritize showing playing players over stopped ones
    - handle play-pause action for currently playing player
    
    Signed-off-by: blacksuan19 <i@blacksuan19.me>

--- bin/bin/playing.sh
+++ bin/bin/playing.sh
@@ -1,29 +1,44 @@
 #!/bin/bash
+P_ICON=""
+S_ICON=""
+PLAYERS=$(playerctl -l)
 
-STATUS=$(playerctl status 2> /dev/null)
+# set current player to the one actually playing or paused
+for player in $PLAYERS; do
+    STATUS=$(playerctl -p $player status 2> /dev/null)
+    if [ "$STATUS" == "Playing" ]; then
+        CURRENT=$player
+        break; #  prioritize playing players
+    elif [ "$STATUS" == "Paused" ]; then
+        CURRENT=$player
+    fi
+done
+if [ $CURRENT == "" ]; then
+echo "  No player is running"
+exit 1;
+fi
+METADATA="$(playerctl -p $CURRENT metadata artist) - $(playerctl -p $CURRENT metadata title)"
+TRIM=$(echo $METADATA | sed -e 's/([^()]*)//g' | cut -c 1-50)
+ARTIST=$(playerctl metadata artist)
+FULL_META=$(playerctl metadata)
+case $STATUS in
+    "Playing")
+        # if spotify is playing on another device artist and song will be empty
+        if [[ "$ARTIST" == "" ]] && [[ "$FULL_META" =~ "spotify" ]]; then
+            echo $P_ICON"  ""Playing on Another Device"
+        else
+            echo $P_ICON"  "$TRIM
+        fi;;
+    "Paused")
+        if [[ "$ARTIST" == "" ]] && [[ "$FULL_META" =~ "spotify" ]]; then
+            echo $S_ICON"  "" Paused on Another Device"
+        else
+            echo $S_ICON"  "$TRIM
+        fi;;
+esac
 
-if [ "$STATUS" != "Playing" ] && [ "$STATUS" != "Paused" ]; then
-    echo "  No player is running"
-else
-    P_ICON=""
-    S_ICON=""
-    METADATA="$(playerctl metadata artist) - $(playerctl metadata title)"
-    TRIM=$(echo $METADATA | sed -e 's/([^()]*)//g' | cut -c 1-50)
-    ARTIST=$(playerctl metadata artist)
-    FULL_META=$(playerctl metadata)
-    case $STATUS in
-        "Playing")
-            # if spotify is playing on another device artist and song will be empty
-            if [[ "$ARTIST" == "" ]] && [[ "$FULL_META" =~ "spotify" ]]; then
-                echo $P_ICON"  ""Playing on Another Device"
-            else
-                echo $P_ICON"  "$TRIM
-            fi;;
-        "Paused")
-            if [[ "$ARTIST" == "" ]] && [[ "$FULL_META" =~ "spotify" ]]; then
-                echo $S_ICON"  "" Paused on Another Device"
-            else
-                echo $S_ICON"  "$TRIM
-            fi;;
-    esac
+# for pause play functionality
+if [ $# -eq 1 ]
+  then
+    playerctl -p $CURRENT play-pause
 fi
