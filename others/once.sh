50b9dca7730b22f451e46f89bc843f65ddbfdca4
Blacksuan19 <abubakaryagob@gmail.com>
2022-03-02 09:02:22 +0800

    once: add brightness fluctuation issue fix
    Signed-off-by: Blacksuan19 <abubakaryagob@gmail.com>

--- others/once.sh
+++ others/once.sh
@@ -2,17 +2,25 @@
 
 # this script should only be run once during the first setup
 
-##
+function fix_brightness() {
+    target="/usr/share/X11/xorg.conf.d/10-quirks.conf"
+
+sudo tee -a $target > /dev/null <<EOT
+# Fix brightness randomly changing
+Section "InputClass"
+        Identifier "Spooky Ghosts"
+        MatchProduct "Video Bus"
+        Option "Ignore" "on"
+EndSection
+EOT
+}
+
 # Pacman Stuff
-##
-# add chaotic and herecura repos
 function setup_repos() {
 sudo cp pacman.conf /etc/pacman.conf
 }
 
-##
 # Git Config
-##
 function setup_git() {
 echo -e "Configuring git..."
 # setup user details
@@ -22,7 +30,6 @@ git config --global user.email abubakaryagob@gmail.com
 # setup gpg signing
 git config --global --add gpg.program /usr/bin/gpg
 git config --global commit.gpgsign true
-
 # setup diff-so-fancy
 git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
 git config --global interactive.diffFilter "diff-so-fancy --patch"
@@ -47,9 +54,7 @@ setup_repos
 # install yay from chaotic-aur
 sudo pacman -S --noconfirm yay
 
-##
 # Install packages
-##
 packages=(git diff-so-fancy stow lsd bat ksuperkey tmux ripgrep duf dust nerd-fonts-jetbrains-mono ferdi-bin picom-jonaburg-git kotatogram-desktop)
 yay -S --noconfirm $packages
 
@@ -58,3 +63,6 @@ setup_git
 # setup prezto
 echo -e "Downloading prezto..."
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto" &> /dev/null
+
+# fix brightness
+fix_brightness
