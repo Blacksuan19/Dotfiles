bbe16f86cfdaea355e4161c2651d96dc7709b06c
blacksuan19 <abubakaryagob@gmail.com>
2025-02-24 16:12:04 -0600

    fusuma: automatically find location of current installation
    ruby version updated are annoyingly stupid
    
    Signed-off-by: blacksuan19 <abubakaryagob@gmail.com>

--- fusuma/.config/fusuma/start.sh
+++ fusuma/.config/fusuma/start.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+cd $(gem env gemdir)/gems/fusuma-$(gem search -e fusuma | grep -oP '\(\K[^\)]+')
 
-fusuma
+./exe/fusuma &
+
+notify-send "Auto start" "Fusuma started!" -i touchpad -a Fusuma
